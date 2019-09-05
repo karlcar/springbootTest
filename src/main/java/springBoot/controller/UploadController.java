383bfcb92a093a998bb49cc6cb4857885b75d7a3
karl0810 <zejia.huang@accenture.com>
2019-09-05 19:03:12 +0800

    er

--- src/main/java/springBoot/controller/UploadController.java
+++ src/main/java/springBoot/controller/UploadController.java
@@ -50,4 +50,42 @@ public class UploadController {
 	}
 	
 	
+	@RequestMapping("/indexs")
+	public String toUploads() {
+		return "uploads";
+	}
+	
+	
+	@RequestMapping(value="/upload/batch",method=RequestMethod.POST)
+	@ResponseBody
+	public String uploadFiles(MultipartFile[] file,HttpServletRequest request) {
+		try {
+			//创建文件在服务器端存放路径
+			String dir = request.getServletContext().getRealPath("/upload");
+			File uploadFile = new File(dir);	
+			if(!uploadFile.exists()) {
+				uploadFile.mkdirs();
+			}
+			//生成文件在服务器端的名字
+			for(int i=0;i<file.length;i++) {
+				
+				String fileSuffix = file[i].getOriginalFilename().substring(file[i].getOriginalFilename().lastIndexOf("."));
+				String fileName = UUID.randomUUID().toString() + fileSuffix;
+				
+				//创建文件存放的路径
+				File files = new File(uploadFile + "/" + fileName);
+				
+				//开始上传
+				file[i].transferTo(files);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "上传失败";
+		} 
+		
+		return "上传成功";
+	}
+	
+	
 }
